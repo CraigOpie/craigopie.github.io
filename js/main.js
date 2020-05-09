11137002d926dc32a91bf5fe31f2aa42672196a7
CraigOpie <CraigOpie@Gmail.com>
2020-05-09 10:51:33 -1000

    Modifying navigation in timeline

--- js/main.js
+++ js/main.js
@@ -5,50 +5,21 @@ sitemap:
 ---
 
 $(document).ready(function () {
-  var newNav = '';
   $('a.panel-button').click(function (e) {
     if ($('.content-wrapper').hasClass('showing')){
-      if (window.location.hash && window.location.hash == '#projects') {
-        if (newNav === 'projects') {
-          $('.content-wrapper').removeClass('animated slideInRight')
-          $('.panel-cover').removeClass('panel-cover--collapsed')
-          $('.panel-cover').css('max-width', '100%')
-          $('.panel-cover').animate({ 'width': '100%' }, 400, swing = 'swing', function () {
-          })
-          $('.content-wrapper').removeClass('showing')
-          window.location.hash = '';
-          parent.location.hash = '';
-          return;
-        }
-      }
-      if (window.location.hash && window.location.hash == '#essays') {
-        if (newNav === 'essays') {
-          $('.content-wrapper').removeClass('animated slideInRight')
-          $('.panel-cover').removeClass('panel-cover--collapsed')
-          $('.panel-cover').css('max-width', '100%')
-          $('.panel-cover').animate({ 'width': '100%' }, 400, swing = 'swing', function () {
-          })
-          $('.content-wrapper').removeClass('showing')
-          window.location.hash = '';
-          parent.location.hash = '';
-          return;
-        }
-      }
+      $('.content-wrapper').removeClass('animated slideInRight')
+      $('.panel-cover').removeClass('panel-cover--collapsed')
+      $('.panel-cover').css('max-width', '100%')
+      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+      $('.content-wrapper').removeClass('showing')
+      window.location.hash = '';
+      parent.location.hash = ''
+      return;
     }
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
-      if (window.location.hash && window.location.hash == '#projects') {
-        if (newNav === 'projects') {
-          $('.panel-cover').addClass('panel-cover--collapsed')
-          $('.content-wrapper').addClass('animated slideInRight')
-        }
-      }
-      if (window.location.hash && window.location.hash == '#essays') {
-        if (newNav === 'essays') {
-          $('.panel-cover').addClass('panel-cover--collapsed')
-          $('.content-wrapper').addClass('animated slideInRight')
-        }
-      }
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
@@ -57,15 +28,11 @@ $(document).ready(function () {
   })
 
   if (window.location.hash && window.location.hash == '#projects') {
-    if (newNav === 'projects') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
+    $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
   if (window.location.hash && window.location.hash == '#essays') {
-    if (newNav === 'essays') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
+    $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
@@ -78,18 +45,20 @@ $(document).ready(function () {
   })
 
   $('.navigation-wrapper .projects-button').click(function () {
-    newNav = 'projects';
-    document.getElementById("projects").style.display = contents;
-    document.getElementById("essays").style.display = none;
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    document.getElementById("projects").style.display = none;
+    document.getElementById("essays").style.display = contents;
+    if (window.location.hash && window.location.hash == '#projects') {
+      $('.navigation-wrapper').toggleClass('visible')
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    }
   })
 
   $('.navigation-wrapper .essays-button').click(function () {
-    newNav = 'essays';
     document.getElementById("projects").style.display = none;
     document.getElementById("essays").style.display = contents;
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    if (window.location.hash && window.location.hash == '#essays') {
+      $('.navigation-wrapper').toggleClass('visible')
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    }
   })
 })
