cdf0af9d27802f3ddff6b67bc07b7ade40c8e86c
CraigOpie <CraigOpie@Gmail.com>
2020-05-09 10:43:19 -1000

    Modifying navigation in timeline

--- js/main.js
+++ js/main.js
@@ -21,6 +21,19 @@ $(document).ready(function () {
           return;
         }
       }
+      if (window.location.hash && window.location.hash == '#essays') {
+        if (newNav === 'essays') {
+          $('.content-wrapper').removeClass('animated slideInRight')
+          $('.panel-cover').removeClass('panel-cover--collapsed')
+          $('.panel-cover').css('max-width', '100%')
+          $('.panel-cover').animate({ 'width': '100%' }, 400, swing = 'swing', function () {
+          })
+          $('.content-wrapper').removeClass('showing')
+          window.location.hash = '';
+          parent.location.hash = '';
+          return;
+        }
+      }
     }
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
