8e724440fd22ee298c4e3fabc867cca1a3019ae0
Craig Opie <craig@deathstardev.com>
2024-09-23 18:25:47 -1000

    Test SOP Addition

--- js/main.js
+++ js/main.js
@@ -35,6 +35,10 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
+  if (window.location.hash && window.location.hash == '#statements') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
